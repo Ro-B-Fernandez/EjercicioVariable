4c83d1420a057f96b005e1ee39bc8f679fb88d38
Ro-B-Fernandez <104107565+Ro-B-Fernandez@users.noreply.github.com>
2022-04-22 22:27:19 -0300

    Initial commit

---
+++ variable.ts
@@ -0,0 +1,31 @@
+//Ejercicio 1
+let paso1 : string = "Receta de torta de chocolate";
+let paso2 : string = "Batir los huevos durante 5 minutos. Añadir el azúcar de a poco y batir un 1 minuto más";
+let paso3 : string = "Agregar el aceite, la leche, la esencia de vainilla y batir nuevamente";
+let paso4 : string = "Añadir los ingredientes secos";
+let paso5 : string = "Agregar el agua bien caliente y mezclalo";
+let paso6 : string = "Enmantecar un molde de 18 cm de diámetro";
+let paso7 : string = "Colocar papel manteca en la base y volver a enmantecar y enharinar";
+let paso8 : string = "Colocar la preparacion en el molde";
+let paso9 : string = "Cocinala en un horno precalentado a 180°C durante 55 minutos";
+let paso10 : string ="¡Se puede acompañar con crema chantilly y frutillas!";
+
+console.log(paso1);
+console.log(paso2);
+console.log(paso3);
+console.log(paso4);
+console.log(paso5);
+console.log(paso6);
+console.log(paso7);
+console.log(paso8);
+console.log(paso9);
+console.log(paso10);
+
+
+//Ejercicio 2
+let base : number = 5;
+let altura : number = 10;
+let resultado : number = base * altura;
+
+
+console.log(resultado);
